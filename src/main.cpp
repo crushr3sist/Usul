3788b930b714ec85c2caa839ace9a65b8469e654
crushr3sist <silent.death3500@gmail.com>
2026-08-26 19:35:55 +1000

    installed xtensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,20 @@
-#include <usul.hpp>
 #include <print>
+#include <usul.hpp>
+#include <xtensor/io/xio.hpp>
+#include <xtensor/containers/xarray.hpp>
+#include <xtensor/views/xview.hpp>
 
 using namespace std;
 
 int main() {
+  xt::xarray<double> arr = {1.0, 2.0, 3.0};
+  println("array: {}", arr.shape());
 
-  const float LR = 0.0001;
+  xt::xarray<double> arr2 = {1.0, 2.0, 3.0};
 
-  auto x = Node::create(random_float());
-  auto y = Node::create(random_float());
-  auto z = Node::create(random_float());
+  xt::xarray<double> result = arr + arr2;
 
-  auto target = Node::create((sin(x->value) / y->value * z->value) + 3);
-
-  vector<Layer> network;
-
-  Layer hidden_layer1(3, 3);
-  Layer hidden_layer2(3, 3);
-  Layer hidden_layer3(3, 3);
-  Layer output(1, 3);
-
-  network.push_back(hidden_layer1);
-  network.push_back(hidden_layer2);
-  network.push_back(hidden_layer3);
-  network.push_back(output);
-
-  for (size_t i = 0; i <= 50; i++) {
-
-    vector input_vector = {x, y, z};
-
-    auto hidden_layer1_output = network[0].forward(input_vector);
-    auto hidden_layer2_output = network[1].forward(hidden_layer1_output);
-    auto hidden_layer3_output = network[2].forward(hidden_layer2_output);
-    auto prediction = network[3].forward(hidden_layer3_output)[0];
-
-    println("EPOCH {}", i);
-    println();
-    println("prediction: {}", prediction->value);
-    println("target: {}", target->value);
-    auto error = (*prediction - *target);
-
-    auto loss = *(*prediction - *target) * *(*prediction - *target);
-
-    println("Loss: {}", loss->value);
-
-    autodiff(loss);
-
-    gradient_descent(network, LR);
-  }
+  println("{}", result);
 
   return 0;
 }

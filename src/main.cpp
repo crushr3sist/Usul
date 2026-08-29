bb0266c7ac1081d56604d3480066b2d474f4476d
crushr3sist <silent.death3500@gmail.com>
2026-08-29 23:50:52 +1000

    dependencies in build-system

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,28 @@
 #include <xtensor/io/xio.hpp>
 #include <xtensor/containers/xarray.hpp>
 #include <xtensor/views/xview.hpp>
+#include <xtensor/generators/xrandom.hpp>
+#include <xtensor-blas/xlinalg.hpp>
 
 using namespace std;
 
+xt::xarray<double> generate_random_matrix(const vector<int> &shape, double low, double high) {
+  auto random_expression = xt::random::rand<double>(shape, low, high);
+  xt::xarray<double> _ = random_expression;
+  return _;
+}
+
 int main() {
-  xt::xarray<double> arr = {1.0, 2.0, 3.0};
-  println("array: {}", arr.shape());
 
-  xt::xarray<double> arr2 = {1.0, 2.0, 3.0};
+  auto Y_true = generate_random_matrix({64, 256}, -5, 5);
 
-  xt::xarray<double> result = arr + arr2;
+  auto X = generate_random_matrix({64, 128}, -5, 5);
+  auto W = generate_random_matrix({128, 256}, -5, 5);
+  auto B = generate_random_matrix({1, 256}, -5, 5);
 
-  println("{}", result);
+  auto Z = xt::linalg::dot(X, W);
+
+  println("{}", Z);
 
   return 0;
 }
